4c9df15436ade66d6055a146024666e5e1c5494e
gumanzanoo <gustavomazanoluiz@hotmail.com>
2023-04-22 23:13:16 -0300

    time execution for ordenation methods

--- src/main/java/br/eu/mavenproject4/Main.java
+++ src/main/java/br/eu/mavenproject4/Main.java
@@ -9,14 +9,23 @@
     public static void main(String[] args) {
         int[] arr = {33, 22, 44, 55, 98, 34, 12, 34, 45, 67};
 
+        long startTime = System.currentTimeMillis();
         Bolha bolha = new Bolha();
         Bolha.bubbleSort(arr);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Tempo de execução do Bubble Sort: " + (endTime - startTime) + " ms");
 
+        startTime = System.currentTimeMillis();
         Insertion insertion = new Insertion();
         insertion.insertionSort(arr);
+        endTime = System.currentTimeMillis();
+        System.out.println("Tempo de execução do Insertion Sort: " + (endTime - startTime) + " ms");
 
+        startTime = System.currentTimeMillis();
         Selection selection = new Selection();
         selection.selectionSort(arr);
+        endTime = System.currentTimeMillis();
+        System.out.println("Tempo de execução do Selection Sort: " + (endTime - startTime) + " ms");
     }
 }
  
